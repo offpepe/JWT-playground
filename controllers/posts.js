10ff4d844c99248e861209af8b0be397fced434a
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-08 09:01:33 -0400

    feat: validation of token when request get endpoint

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+const fs = require('fs').promises;
+
 const mockPosts = [
   {
     title: 'título fake',
@@ -17,6 +19,9 @@ const mockPosts = [
   },
 ];
 
-module.exports = (_req, res) => {
+module.exports = async (req, res) => {
+  const token = await fs.readFile('./token.txt', 'utf-8');
+  const auth = req.headers.authorization
+  if(!auth || auth !== token ) return res.status(401).json({ error: { code: 401, message: 'é necessário efetuar login para fazer este tipo de operação!' } });
   res.status(200).json({ mockPosts });
 };
