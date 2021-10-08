a934ab53f776134f44e539eac324f37b5936a6f9
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-08 09:37:53 -0400

    refactor: changing use of bodyparser to use express.json and express.urlencoded

--- api/server.js
+++ api/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const apiRoutes = express.Router();
 
--- controllers/posts.js
+++ controllers/posts.js
@@ -21,7 +21,7 @@ const mockPosts = [
 
 module.exports = async (req, res) => {
   const token = await fs.readFile('./token.txt', 'utf-8');
-  const auth = req.headers.authorization
+  const auth = req.headers.authorization;
   if(!auth || auth !== token ) return res.status(401).json({ error: { code: 401, message: 'é necessário efetuar login para fazer este tipo de operação!' } });
   res.status(200).json({ mockPosts });
 };
